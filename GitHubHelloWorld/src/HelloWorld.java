dfd7e7911b521160ebc7d754a8520447ffdafaf7
huskdoll <huskdoll@gmail.com>
2012-10-22 11:34:13 +0900

    first commit

---
+++ GitHubHelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld();
+		hw.ppp("HelloWorld");
+	}
+
+	public void ppp(Object o){
+		System.out.println(o.toString());
+	}
+	
+
+}
